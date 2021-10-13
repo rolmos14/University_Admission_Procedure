786630d126d0f7552e845f88fad085e49f823713
rolmos14 <olmos.rafa@gmail.com>
2021-10-13 23:32:01 +0200

    Stage 1/7. Initial solution, just an input of three scores and mean calcultaion.

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -1,1 +1,19 @@
-# write your code here
+class Admission:
+
+    def __init__(self):
+        self.scores = []
+
+    def submit_scores(self):
+        self.scores = [int(input()) for _ in range(3)]
+
+    def mean_score(self):
+        return sum(self.scores) / len(self.scores)
+
+    def print_result(self):
+        print(self.mean_score())
+        print("Congratulations, you are accepted!")
+
+
+candidate = Admission()
+candidate.submit_scores()
+candidate.print_result()
