000c218ea4837cd32509b157163bfeac861a99c0
rolmos14 <olmos.rafa@gmail.com>
2021-10-13 23:40:07 +0200

    Stage 2/7. New method accepted(). Print_result() updated accordingly.

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -9,9 +9,15 @@ class Admission:
     def mean_score(self):
         return sum(self.scores) / len(self.scores)
 
+    def accepted(self):
+        return self.mean_score() >= 60.0
+
     def print_result(self):
         print(self.mean_score())
-        print("Congratulations, you are accepted!")
+        if self.accepted():
+            print("Congratulations, you are accepted!")
+        else:
+            print("We regret to inform you that we will not be able to offer you admission.")
 
 
 candidate = Admission()
