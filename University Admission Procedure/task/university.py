720ceabd5799ff9780d8d5762ca65a76830b858e
rolmos14 <olmos.rafa@gmail.com>
2021-10-20 22:54:43 +0200

    Stage 4/7. submit_applicants now gets applicants from a text file. POINTS 1 & 2 OF THE STAGE ALREADY COVERED & TESTED.

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -1,14 +1,19 @@
 class Admission:
 
-    def __init__(self, applicants, accepted):
+    def __init__(self, max_accepted):
         self.applicants = []
-        self.total_applicants = applicants
-        self.total_accepted = accepted
+        self.max_accepted = max_accepted
 
-    def submit_applicants(self):
-        for _ in range(self.total_applicants):
-            first_name, last_name, gpa = input().split()
-            self.applicants.append([first_name, last_name, float(gpa)])
+    def submit_applicants(self, file_name):
+        # Read the file with one applicant per line
+        with open(file_name) as file:
+            file_lines = file.read().splitlines()  # list with lines without '\n'
+        # Store applicants as nested lists with 6 elements per applicant:
+        # [['first_name', 'last_name', GPA, '1st_department', '2nd_department', '3rd_department'], ['first_name', ...]]
+        for applicant in file_lines:
+            first_name, last_name, gpa, dep_1, dep_2, dep_3 = applicant.split()
+            self.applicants.append([first_name, last_name, float(gpa), dep_1, dep_2, dep_3])
+        print(self.applicants)
 
     def accepted_applicants(self):
         print("Successful applicants:")
@@ -18,6 +23,6 @@ class Admission:
             print(first_name, last_name)
 
 
-admission = Admission(int(input()), int(input()))
-admission.submit_applicants()
+admission = Admission(int(input()))
+admission.submit_applicants("applicant_list.txt")
 admission.accepted_applicants()
