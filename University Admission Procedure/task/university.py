56592f73ddde603c845c4d70dd92cb9e59e0523a
rolmos14 <olmos.rafa@gmail.com>
2021-10-21 00:00:45 +0200

    Stage 4/7. submit_applicants now gets applicants from a text file. NEED TO REVIEW SORTING ALGORITHM: FIRST GPA + FULL NAME, BUT THEN WE NEED TO CHECK BY DEPARTMENT WHICH APPLICANTS HAVE THE DEPARTMENT AS FIRST OPTION. THEN SECOND PRIORITIES. FOCUS ON THE ALGORITHM DESCRIPTION OF THE EXERCISE

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -13,14 +13,25 @@ class Admission:
         for applicant in file_lines:
             first_name, last_name, gpa, dep_1, dep_2, dep_3 = applicant.split()
             self.applicants.append([first_name, last_name, float(gpa), dep_1, dep_2, dep_3])
-        print(self.applicants)
 
     def accepted_applicants(self):
-        print("Successful applicants:")
+        # Sort applicants with high GPA first. If GPA is the same, sort by full name
         sorted_applicants = sorted(self.applicants, key=lambda x: (-x[2], x[0], x[1]))
-        for applicant in sorted_applicants[:self.total_accepted]:
-            first_name, last_name = applicant[0], applicant[1]
-            print(first_name, last_name)
+        departments = sorted(['Mathematics', 'Physics', 'Biotech', 'Chemistry', 'Engineering'])
+        departments_lists = [[] for _ in departments]  # initialize departments lists
+        # Loop through sorted_applicants to assign each applicant to its first option, second if first is full and so on
+        for applicant in sorted_applicants:
+            for department in applicant[3:6]:
+                if len(departments_lists[departments.index(department)]) < self.max_accepted:
+                    # Store 'first_name' + 'last_name' + GPA for each applicant
+                    departments_lists[departments.index(department)].append(applicant[:3])
+                    break
+        # Print departments and their accepted students
+        for idx, department in enumerate(departments):
+            print('\n' + department)
+            for student in departments_lists[idx]:
+                # 'first_name' + 'last_name' + GPA
+                print(student[0], student[1], student[2])
 
 
 admission = Admission(int(input()))
